46a709e95099d8751265f97260df649a9e0d509e
BDiaz64 <bryanwdiaz64@gmail.com>
2024-05-30 19:24:13 -0400

    Created User and Credit Card classes
    We'll eventually need these classes for the user login feature, the shopping cart, and the wishlist feature

---
+++ bookstore/src/main/java/app/bookstore/CreditCard.java
@@ -0,0 +1,67 @@
+package src.main.java.app.bookstore;
+
+public class CreditCard 
+{
+	String myCardHolder; //Name of the card holder
+	String myCardNumber; //The digits in groups of 4
+	int myExpirationMonth; // 01 - 12
+	int myExpirationYear; // Two digit year (24 for 2024, 25 for 2025, etc)
+	int myCVC; // The three funny numbers on the back
+	
+	//*Constructors go here*
+
+	//*Constructors go here*
+	
+	// Getters
+	String getCardHolder()
+	{
+		
+	}
+	
+	String getCardNumber()
+	{
+		
+	}
+	
+	int getExpirationMonth()
+	{
+		
+	}
+	
+	int getExpirationYear()
+	{
+		
+	}
+	
+	int getCVC()
+	{
+		
+	}
+	
+	// Setters
+	void setCardHolder(String cardHolder)
+	{
+		
+	}
+	
+	void setCardNumber (String cardNumber)
+	{
+		
+	}
+	
+	void setExpirationMonth (int expirationMonth)
+	{
+		
+	}
+	
+	void setExpirationYear (int expirationYear)
+	{
+		
+	}
+	
+	void setCVC (int cvc)
+	{
+		
+	}
+}
+
--- bookstore/src/main/java/app/bookstore/User.java
+++ bookstore/src/main/java/app/bookstore/User.java
@@ -1,6 +1,101 @@
 package app.bookstore;
 
+import java.util.List;
+
 public class User 
 {
+    private String myUserID;
+    private String myUsername;
+    private String myPassword;
+    private String myEmailAddress;
+    private String myHomeAddress;
+    private List<Book> myWishlist;
+    private List<Book> myShoppingCart;
+    private CreditCard myCreditCard;
+    
+    //*Constructors go here*
 
+  	//*Constructors go here*
+    
+    // Getters
+    public String getUserID() 
+    {
+        return myUserID;
+    }
+
+    public String getUsername() 
+    {
+        return myUsername;
+    }
+
+    public String getPassword() 
+    {
+        return myPassword;
+    }
+
+    public String getEmailAddress() 
+    {
+        return myEmailAddress;
+    }
+
+    public String getHomeAddress() 
+    {
+        return myHomeAddress;
+    }
+
+    public List<Book> getWishlist() 
+    {
+        return myWishlist;
+    }
+    
+    public List<Book> getShoppingCart() 
+    {
+        return myShoppingCart;
+    }
+
+    public CreditCard getCreditCard() 
+    {
+        return myCreditCard;
+    }
+    
+    // Setters
+    public void setUserID(String userID) 
+    {
+        myUserID = UserID;
+    }
+
+    public void setUsername(String username) 
+    {
+        myUsername = username;
+    }
+
+    public void setPassword(String password) 
+    {
+        myPassword = password;
+    }
+
+    public void setEmailAddress(String emailAddress) 
+    {
+        myEmailAddress = emailAddress;
+    }
+
+    public void setHomeAddress(String myHomeAddress) 
+    {
+        myHomeAddress = myHomeAddress;
+    }
+
+    public void setWishlist(List<Book> wishlist) 
+    {
+        myWishlist = wishlist;
+    }
+    
+    public void setShoppingCart(List<Book> shoppingCart) 
+    {
+        myShoppingCart = ShoppingCart;
+    }
+
+    public void setCreditCard(CreditCard creditCard) 
+    {
+        myCreditCard = creditCard;
+    }
 }
